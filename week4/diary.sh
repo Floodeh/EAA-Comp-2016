122b353b35d747dd0b53820921ca958135ee6922
Adam Flood <adamflood888@hotmail.com>
2016-10-14 10:03:17 +0100

    refactored to parameterise the diary filename

--- week4/diary.sh
+++ week4/diary.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+diaryFile=myDiary.txt
 read diaryEntry
 #redirect >> append symbol
 
-echo $( date ) $diaryEntry >> myDiary.txt
+echo $( date ) $diaryEntry >> $diaryFile
 
 #alternative syntax
 #echo `date` $text >> myDiary.txt
